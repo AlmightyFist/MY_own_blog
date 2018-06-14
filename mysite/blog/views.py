6fc2f09ff48e46abf156eab21724d91768500eb2
AlmightyFist <marcingrabarczyk93@gmail.com>
2018-06-14 15:06:10 +0200

    My_own_blog, working urls

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,5 +3,8 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):# wyświetla listę WSZYSTKICH postów
-    posts = Post.objects.filter(status='published').order_by('-publish')
+    posts = Post.objects.filter(status='published')
     return render(request, 'blog/post_list.html', {'posts':posts} )
+
+def main_site(request):
+    return render(request, 'blog/main_site.html', {})
